af4cb5294bc16daa893ff593d0a5f5e8e1365b4b
Muhammad Burhan Shahzad <burishahzada7728@gmail.com>
2025-02-06 18:16:56 +0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+  images: {
+    domains: ['not-lain-background-removal.hf.space',
+      'res.cloudinary.com'
+    ],
+  },
 };
 
 export default nextConfig;
